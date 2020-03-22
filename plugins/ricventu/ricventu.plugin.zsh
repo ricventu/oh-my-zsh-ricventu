8564c003bce0c69c1bd81f8b2d0fa67a1a94e553
Riccardo Venturini <riccardo@fornacestudio.com>
2020-03-22 12:27:06 +0100

    new homestead

--- plugins/ricventu/ricventu.plugin.zsh
+++ plugins/ricventu/ricventu.plugin.zsh
@@ -3,13 +3,17 @@ export NPM_PACKAGES="${HOME}/.npm-packages"
 export PATH="$NPM_PACKAGES/bin:/usr/local/bin:$PATH"
 
 function homestead() {
-    ( cd ~/code/homestead && vagrant $* )
+    ( cd ~/code/github/homestead && vagrant $* )
 }
 
 alias zshrc='code ~/.zshrc "$ZSH" "$ZSH_CUSTOM"'
 
 alias hosts='code /etc/hosts'
-alias homesteadrc='code ~/code/homestead/Homestead.yaml /etc/hosts'
+alias homesteadrc='code ~/code/github/homestead/Homestead.yaml /etc/hosts'
 alias homesteadssh='homestead ssh'
 alias a='php artisan'
 alias flow='git flow'
+alias flow_feature_start='git flow feature start' 
+alias flow_feature_end='git flow feature finish' 
+alias flow_fix_start='git flow hotfix start' 
+alias flow_fix_end='git flow hotfix finish' 
