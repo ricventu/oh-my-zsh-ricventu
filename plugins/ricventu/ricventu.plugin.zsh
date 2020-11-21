4d6f06efd042c4262801f563ad4649cd838ca1aa
Riccardo Venturini <riccardo@fornacestudio.com>
2020-11-21 13:57:01 +0100

    wip

--- plugins/ricventu/ricventu.plugin.zsh
+++ plugins/ricventu/ricventu.plugin.zsh
@@ -10,17 +10,24 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 function homestead() {
-    ( cd ~/code/github/homestead && vagrant $* )
+    ( cd ~/code/homestead && vagrant $* )
 }
+alias homesteadrc='code ~/code/homestead/Homestead.yaml /etc/hosts'
+alias homesteadssh='homestead ssh'
+
+# heroku autocomplete:script zsh
+
 
 alias zshrc='code ~/.zshrc "$ZSH" "$ZSH_CUSTOM"'
-
-alias hosts='code /etc/hosts'
-alias homesteadrc='code ~/code/github/homestead/Homestead.yaml /etc/hosts'
-alias homesteadssh='homestead ssh'
 alias a='php artisan'
 alias flow='git flow'
 alias flow_feature_start='git flow feature start' 
 alias flow_feature_end='git flow feature finish' 
 alias flow_fix_start='git flow hotfix start' 
 alias flow_fix_end='git flow hotfix finish' 
+
+alias lara='code ~/code/laradock'
+function did() {
+    ( cd ~/code/dev-in-docker-php && docker-composer $* )
+}
+alias didrc='code ~/code/dev-in-docker-php'
