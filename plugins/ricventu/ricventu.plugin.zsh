f7e6dd4b1cb89195437b8363f0867e4cea3c521b
Riccardo Venturini <riccardo@fornacestudio.com>
2022-02-04 11:44:38 +0100

    composer

--- plugins/ricventu/ricventu.plugin.zsh
+++ plugins/ricventu/ricventu.plugin.zsh
@@ -11,23 +11,21 @@ export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
 export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
 
-# NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-
 # heroku autocomplete:script zsh
-
 
 alias zshrc='code ~/.zshrc "$ZSH" "$ZSH_CUSTOM"'
 alias a='php artisan'
 
+function composer() {
+    docker run --rm --interactive --tty \     
+    --volume $(pwd):/workdir \
+    composer
+}
 
-# ALIAS
 function did() {
     ( cd ~/code/dev-in-docker && ./did $* )
 }
+
 function didphp() {
     did run --rm -v $(pwd):/workdir/ -w /workdir/ php7.3 php $@
 }
@@ -38,4 +36,8 @@ alias didrc='code ~/code/dev-in-docker /etc/hosts'
 
 function strooka() {
     ( cd ~/code/strooka/master/strooka-docker && ./strooka $@ )
+}
+
+function mfb() {
+    ( cd ~/code/myfactorybook/myfactorybook-docker && ./mfb $* )
 }
