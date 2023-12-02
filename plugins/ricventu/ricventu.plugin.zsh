c1347cdaa30b49611ba28aaa2a3933eef4c56faa
Riccardo Venturini <riccardo@fornacestudio.com>
2023-12-02 11:08:10 +0100

    up

--- plugins/ricventu/ricventu.plugin.zsh
+++ plugins/ricventu/ricventu.plugin.zsh
@@ -4,6 +4,8 @@ export EDITOR=vim
 export COMPOSER_MEMORY_LIMIT=-1
 
 # HOMEBREW
+# brew install ncdu fzf bat exa
+
 export HOMEBREW_PREFIX="/opt/homebrew";
 export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
 export HOMEBREW_REPOSITORY="/opt/homebrew";
@@ -11,10 +13,19 @@ export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
 export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
 
+alias cat='bat'
+alias ls='exa --group-directories-first'
+alias ll='exa -l --group-directories-first'
+alias l='exa -l --group-directories-first'
+alias la='exa -la --group-directories-first'
+alias f='fzf'
+alias fzf='fzf --preview "bat --color=always --style=header,grid --line-range :500 {}"'
+
 alias zshrc='code ~/.zshrc "$ZSH" "$ZSH_CUSTOM"'
 alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
-alias a='sail artisan'
 alias didrc='code ~/code/dev-in-docker /etc/hosts'
+
+alias docker-stop-all='dsta'
 
 export PATH="/Users/ricventu/Library/Application Support/JetBrains/Toolbox/scripts:$PATH"
 
@@ -46,7 +57,7 @@ function php81() {
         docker-apps:php-8.1 php $@
 }
 
-function php81ssh() {
+function php81sh() {
     docker run --rm --interactive --tty \
         --user $(id -u):$(id -g) \
         --volume "$(pwd)":/app \
@@ -65,7 +76,7 @@ function composer() {
      docker run --rm --interactive --tty \
         --volume "$(pwd)":/app \
         -w /app \
-        composer composer $@
+        composer:2 $@
 }
 
 function phpsh() {
@@ -75,9 +86,17 @@ function phpsh() {
         docker-apps:php-8.2 bash
 }
 
+function php81buildpack() {
+    docker run --rm --interactive --tty \
+        --volume "/Users/ricventu/code/github/heroku-buildpack-php":/buildpack \
+        --volume "$(pwd)":/app \
+        -w /app \
+        docker-apps:php-8.1 bash
+}
+
 function phpserve() {
     docker run --rm --interactive --tty \
-        --volume "$(pwd)":/app \
+        --volume "$(pwd)":/buildpack \
         -w /app \
         -p '8000:8000' \
         docker-apps:php-8.2 php artisan serve --host 0.0.0.0
@@ -98,6 +117,7 @@ function git-remote-merged-prune() {
 
 function buildpack() {
     # buildpack build my/image --builder heroku/builder:20
+    # buildpack build "my/image:latest" --builder 'heroku/builder:22'  --buildpack 'heroku/php' --buildpack 'heroku/nodejs'
     docker run --rm \
         -v /var/run/docker.sock:/var/run/docker.sock \
         -v $PWD:/workspace -w /workspace \
